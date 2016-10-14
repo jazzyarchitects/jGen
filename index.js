ace867559dfe20890f9e4802f45a5f744ca72f4e
Jibin Mathews <jibinmathews7@gmail.com>
2016-10-15 00:11:59 +0530

    Added minimist

--- index.js
+++ index.js
@@ -1,37 +1,56 @@
 #!/usr/bin/env node
 "use strict";
 
-var chalk = require('chalk');
 
 if(require.main === module){
+  var chalk = require('chalk');
+  var argv = require('minimist')(process.argv);
+  var ip = argv._.splice(2);
+
+  global.color = argv.c || argv.color;
+  console.log(argv);
   //Cli
   /**
     process.argv[0] = "/usr/bin/nodejs"
     process.argv[1] = "/usr/bin/jgen";
 
-  */
+    */
 
-process.on('unhandledRejection', (reason, p) => {
-    console.log(chalk.red("Unhandled Rejection at: Promise "), p, chalk.red(" reason: "), chalk.red(reason));
-});
+    process.on('unhandledRejection', (reason, p) => {
+      console.log(chalk.red("Unhandled Rejection at: Promise "), p, chalk.red(" reason: "), chalk.red(reason));
+    });
 
-process.on('unhandledException', (error, m)=> {
-  console.log(chalk.red("Unhandled Exception at: Error "), m, chalk.red(" reason: "), chalk.red(error));
-})
+    process.on('unhandledException', (error, m)=> {
+      console.log(chalk.red("Unhandled Exception at: Error "), m, chalk.red(" reason: "), chalk.red(error));
+    })
 
-  var jgen = require('./lib');
-  if(process.argv[2]==="init"){
-    jgen.setup();
-  }else if(process.argv[2]==="fix" || process.argv[2]==="update"){
-    jgen.fix();
-  }else if(process.argv[2]==="model"){
-    if(!process.argv[3]){
-      throw new Error("Model name cannot be empty");
+    var jgen = require('./lib');
+
+    if(ip[0]==="init"){
+      jgen.setup();
+    }else if(ip[0]==="fix" || ip[0]==="update"){
+      jgen.fix();
+    }else if(ip[0]==="model"){
+      if(!ip[1]){
+        jgen.showModels();
+      }
+      if(ip[1].toLowerCase()==="remove"){
+        var arr = ip.splice(2, process.argv.length);
+        jgen.removeModels(arr);
+      }else{
+        jgen.model(ip[1]);
+      }
+    }else if(ip[0]==="tree"){
+      var arr = ip.splice(1, process.argv.length);
+      if(arr.length>0){
+        jgen.tree(arr);
+      }else{
+        jgen.tree();
+      }
     }
-    jgen.model(process.argv[3]);
-  }
 
-}else{
-  exports.apiAuthentication = require('./lib/scripts/api-authentication');
-}
+  }else{
+    exports.apiAuthentication = require('./lib/scripts/api-authentication');
+    exports.BaseUserSchema = require('./lib/schemas/user');
+  }
 

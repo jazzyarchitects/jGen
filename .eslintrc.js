c5464b3ce573f22eb32f4880f74676b1937fc244
Jibin Mathews <jibinmathews7@gmail.com>
2016-12-13 22:56:57 +0530

    Adding ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "extends": "standard",
+    "plugins": [
+        "standard",
+        "promise"
+    ],
+    "env":{
+        "node": true,
+        "es6": true
+    },
+    "rules": {
+        "semi": 0,
+        "quotes": 0,
+        "arrow-spacing": 0,
+        "no-undef": 1
+    }
+};
